13eadc1c794ad4371bc813e512041843f8c37593
Onix <piyvoda20033@gmail.com>
2026-06-06 12:49:37 +0300

    fixed cookie options

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -1,16 +1,24 @@
 // Function to setup cookies to the session
 
 export const setupSession = (res, session) => {
-  res.cookie('accessToken', session.accessToken, {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  const cookieOptions = {
     httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+  };
+
+  res.cookie('accessToken', session.accessToken, {
+    ...cookieOptions,
     expires: session.accessTokenValidUntil,
   });
   res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
+    ...cookieOptions,
     expires: session.refreshTokenValidUntil,
   });
   res.cookie('sessionId', session._id, {
-    httpOnly: true,
+    ...cookieOptions,
     expires: session.refreshTokenValidUntil,
   });
 };
