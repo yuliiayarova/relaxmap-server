040ac06bf84fdfedd9a0f4720969d3eefefa8697
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-06-15 16:04:03 +0300

    add multer error for upload img

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,17 +1,31 @@
-import { HttpError } from "http-errors";
+import { HttpError } from 'http-errors';
+import multer from 'multer';
 
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      return res.status(413).json({
+        error: 'FILE_TOO_LARGE',
+        message: 'Image size must not exceed 2 MB',
+      });
+    }
+
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
     });
   }
 
-  const isProd = process.env.NODE_ENV === "production";
+  const isProd = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
     message: isProd
-      ? "Something went wrong. Please try again later."
+      ? 'Something went wrong. Please try again later.'
       : err.message,
   });
 };
