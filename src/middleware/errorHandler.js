d4d3924cb06171c9979d2a1521d6af02ae327584
vjosik <108922728+vjosik@users.noreply.github.com>
2026-06-16 12:06:05 +0300

    fixed swagger 500

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,17 +1,12 @@
 import { HttpError } from "http-errors";
 
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    return res.status(err.status).json({
-      message: err.message || err.name,
-    });
-  }
+  const status = err.status || err.statusCode || 500;
 
-  const isProd = process.env.NODE_ENV === "production";
-
-  res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
+  res.status(status).json({
+    message:
+      status === 500
+        ? 'Something went wrong. Please try again later.'
+        : err.message,
   });
 };
